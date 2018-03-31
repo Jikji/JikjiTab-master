38e655ac933bb16a1b32c957058e98075aeda76c
Jikji <shken35@gmail.com>
2018-04-01 03:58:31 +0900

    ADbanner cocmmit

---
+++ app/src/main/java/com/example/gigabyte/CoupleByDesigner/ui/FragmentAd1.java
@@ -0,0 +1,10 @@
+package com.example.gigabyte.CoupleByDesigner.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Park on 2018-03-31.
+ */
+
+public class FragmentAd1 extends Fragment {
+}

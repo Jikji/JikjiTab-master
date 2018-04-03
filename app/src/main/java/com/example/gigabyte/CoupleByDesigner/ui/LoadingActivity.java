d4f73b64d586d2602009006792760c473ee8c62a
Jikji <shken35@naver.com>
2018-04-03 14:47:42 +0900

    ui 배치 및 skip 구현

--- app/src/main/java/com/example/gigabyte/CoupleByDesigner/ui/LoadingActivity.java
+++ app/src/main/java/com/example/gigabyte/CoupleByDesigner/ui/LoadingActivity.java
@@ -1,12 +1,15 @@
 package com.example.gigabyte.CoupleByDesigner.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
@@ -16,9 +19,12 @@ public class LoadingActivity extends Activity {
 
     private ImageView mImageViewAniLoading;
     private ImageView mImageViewText;
+    private Button mBtnLoadingNext;
+    private RelativeLayout mGroupLoadingWidget;
     private GlideDrawableImageViewTarget gifImage;
     private Animation mAnimationFadeout;
     private Animation mAnimationMoveY;
+    private Animation mAnimationFadeIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,16 @@ public class LoadingActivity extends Activity {
         mImageViewAniLoading = (ImageView) findViewById(R.id.iv_ani_loading);
         gifImage = new GlideDrawableImageViewTarget(mImageViewAniLoading);
         Glide.with(this).load(R.drawable.ani_loading_logo).into(gifImage);
+
+        mBtnLoadingNext = (Button) findViewById(R.id.btn_loading_next);
+        mBtnLoadingNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 //        Handler handler = new Handler() {
 //            public void handleMessage(Message msg) {
 //                super.handleMessage(msg);
@@ -38,16 +54,15 @@ public class LoadingActivity extends Activity {
 //        };
 //        handler.sendEmptyMessageDelayed(0, 5000);
 
-        aniFadeout();
+        FadeOutAni();
     }
 
-    private void aniFadeout() {
-//        mImageViewAniLoading.setAlpha(1.0f);
+    private void FadeOutAni() {
         mImageViewText = (ImageView) findViewById(R.id.iv_text_loading);
         mAnimationMoveY = AnimationUtils.loadAnimation(this, R.anim.translate);
+        mAnimationMoveY.setFillAfter(true);
         mAnimationFadeout = AnimationUtils.loadAnimation(this, R.anim.fadeout);
         mAnimationFadeout.setFillAfter(true);
-        mAnimationMoveY.setFillAfter(true);
         mAnimationFadeout.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -57,6 +72,8 @@ public class LoadingActivity extends Activity {
             @Override
             public void onAnimationEnd(Animation animation) {
                 mImageViewAniLoading.setVisibility(View.INVISIBLE);
+                mImageViewAniLoading.setAlpha(0.0f);
+                FadeInEditText();
             }
 
             @Override
@@ -71,7 +88,38 @@ public class LoadingActivity extends Activity {
             public void run() {
                 mImageViewAniLoading.startAnimation(mAnimationFadeout);
                 mImageViewText.startAnimation(mAnimationMoveY);
-                }
+            }
         }, 5000);
     }
+
+    private void FadeInEditText() {
+        mGroupLoadingWidget = (RelativeLayout) findViewById(R.id.group_loading_widget);
+        mAnimationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        mAnimationFadeIn.setFillAfter(true);
+        mAnimationFadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mGroupLoadingWidget.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                mGroupLoadingWidget.startAnimation(mAnimationFadeIn);
+            }
+        });
+    }
 }
+
